d8871b8c37d7fa83389093c88a5c15401cabec0f
Gabriel Demetrios Lafis <33261275+galafis@users.noreply.github.com>
2026-03-18 10:35:37 -0300

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+"""COVID-19 ML Prediction Pipeline.
+
+A production-ready machine learning pipeline for predicting
+COVID-19 case evolution in Brazil using real public data.
+"""
+
+__version__ = "1.0.0"
+__author__ = "Gabriel Demetrios Lafis"
